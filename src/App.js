862ed36d9df7d4571bca332e9e4735fe8ff60e0b
systemadmin-lab <rejuananik@gmail.com>
2025-07-15 04:10:03 +0600

    compiled with 4 bug

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
+import {Provider} from "react-redux"
+import store from "./redux/store";
 
 function App() {
     return (
+        <Provider store={store}>
         <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
             <Navbar />
 
@@ -20,6 +23,7 @@ function App() {
                 <Footer />
             </div>
         </div>
+        </Provider>
     );
 }
 
