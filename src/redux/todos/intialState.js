5bc593fa6d2ffc78312bb2115dad516d7c703b92
systemadmin-lab <rejuananik@gmail.com>
2025-07-15 01:41:34 +0600

    reducer done

---
+++ src/redux/todos/intialState.js
@@ -0,0 +1,12 @@
+export const intialState =[
+    {
+    id:1,
+    text:"learn react js",
+    completed:true
+},
+{
+    id:2,
+    text:"learn redux",
+    completed:false
+}
+]
---
+++ src/redux/todos/reducer.js
@@ -0,0 +1,39 @@
+import { intialState } from "./intialState"
+import {ADDED,ALLCOMPLETED,CLEARCOMPLETED,COLORSELECTED,TOGGLED,DELETED} from "./actiontype"
+
+const nextTodoId = (todos) =>{
+    return todos.length > 0
+    ?Math.max(...todos.map(todo =>todo.id)+1)
+    :1
+     
+}
+
+const reducer =(state=intialState, action) =>{
+    switch(action.type){
+        case ADDED:
+            return[
+                ...state,
+                {
+                   id:nextTodoId(state)
+                }
+            ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            default:
+                break;
+        
+    }
+}
